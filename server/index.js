ec779f1e29dd225156fc96087c1a4b2c20dbc518
NOEL318 <noelrinaya318@gmail.com>
2025-04-07 18:08:34 -0600

    Fix .env issue

--- server/index.js
+++ server/index.js
@@ -3,28 +3,22 @@ const cors = require("cors");
 var mysql = require("mysql");
 const util = require("util");
 
-
-
+const app = express();
+require("dotenv").config();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+const PORT = process.env.PORT || 5001;
 
 var sql = mysql.createConnection({
   host: process.env.HOSTNAME_BD,
   port: 21811,
-  user: process.env.USERBD,
+  user: process.env.USER_BD,
   password: process.env.PASSWORD_BD,
   database: process.env.DATABASE_DB,
 });
 
 sql.connect();
-
-const app = express();
-
-require("dotenv").config();
-
-app.use(cors());
-app.use(express.json({ limit: "50mb" }));
 const dbquery = util.promisify(sql.query).bind(sql);
-
-const PORT = process.env.PORT || 5001;
 
 app.get("/api/hi", async (req, res) => {
   const results = await dbquery("Select * from Productos;");
